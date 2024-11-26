65cd8ee2e9d48d96d2eb02e79e6e8ffedc64c91f
usuariodebruno <usuariodebruno@gmail.com>
2024-11-26 16:54:14 -0300

    Add: hooks effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Counter from './Counter2';
 
+
 function App() {
-  return (
-    <Counter count={5}></Counter>
-  );
+
+  const [show, setShow] = useState(false);
+
+  let time = 0;
+
+  useEffect(() => {
+    
+    let timer = setInterval(() => {
+      time++;
+      console.log(time);
+      if (time > 10) {
+        clearInterval(timer);
+      }
+    }, 1000);
+
+  }, []);
+
+
+  if (show) {
+    return (
+      <Counter count={5}></Counter>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Contador desmontado</h1>
+        <button onClick={() => setShow(true)}>Mostrar contador</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/Counter2.js
+++ src/Counter2.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Counter(props) {
     const[count, setCount] = useState(props.count); // inicializa o estado com o valor de props.count
 
+    // Com o array vazio, o useEffect é chamado apenas uma vez, quando o componente é montado
+    useEffect(() => {
+        setCount(parseInt(localStorage.getItem("count")));
+
+        // Isso só será executado quando o componente for desmontado
+        return () => {
+            console.log("componente desmontado");
+        }
+    }, [])
+
+    // Com o array de dependências, o useEffect é chamado toda vez que a variável count é alterada
+    useEffect(()=> {
+        document.title = count;
+        localStorage.setItem("count", count);
+    }, [count])
+
+    // Função para incrementar o contador
     function increment() {
         setCount(count + 1);
         console.log("increment");
     }
 
+    // Função para decrementar o contador
     function decrement() {
         setCount(count - 1);
         console.log("decrement");
